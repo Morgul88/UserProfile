467c013b84bbef2869ca26d1307fc9144c19cce6
Henrik <staranderh@gmail.com>
2024-02-09 10:31:23 +0100

    Klar

--- Infrastructure/Service/UserProfileService.cs
+++ Infrastructure/Service/UserProfileService.cs
@@ -23,19 +23,8 @@ public class UserProfileService(UserProfileRepository userProfileRepository, Rol
             
             if (!_userProfileRepository.Exists(x => x.Id == user.Id))
             {
-                //Test
-                //var adminRole = _roleRepository.GetOne(x => x.RoleType == "Admin");
-                //var privateRole = _roleRepository.GetOne(x => x.RoleType == "Private");
-
-                // Skapa rollerna om de inte redan finns
-                //adminRole ??= _roleRepository.Create(new RoleEntity { RoleType = "Admin" });
-                //privateRole ??= _roleRepository.Create(new RoleEntity { RoleType = "Private" });
-
-                //var roleEntity = _roleRepository.GetOne(x => x.Id == user.Id);
-                //roleEntity ??= _roleRepository.Create(new RoleEntity
-                //{
-                //    RoleType = user.RoleType
-                //});
+                
+                
 
                 RoleEntity roleEntity;
 
@@ -132,7 +121,7 @@ public class UserProfileService(UserProfileRepository userProfileRepository, Rol
         catch (Exception ex)
         {
             Debug.WriteLine("ERROR :: " + ex.Message);
-            // Hantera exception på ett lämpligt sätt, beroende på ditt scenarie
+            
             return false;
         }
 
@@ -147,10 +136,10 @@ public class UserProfileService(UserProfileRepository userProfileRepository, Rol
 
             if (userProfileEntity != null)
             {
-                // Ta bort UserProfileEntity, vilket kan orsaka kaskad borttagning av associerade entiteter
+               
                 _userProfileRepository.Delete(x => x.Id == userId);
 
-                // Alternativt kan du explicit ta bort associerade entiteter separat om de inte kaskaderas
+                
                 var adressEntity = _adressRepository.GetOne(x => x.Id == userProfileEntity.AdressId);
                 _adressRepository.DeleteByEntity(adressEntity);
 
@@ -193,7 +182,7 @@ public class UserProfileService(UserProfileRepository userProfileRepository, Rol
         catch (Exception ex)
         {
             Debug.WriteLine("ERROR :: " + ex.Message);
-            // Du bör överväga att logga felet istället för att bara skriva ut det till Debug-fönstret.
+            
         }
 
         return null!;
@@ -212,9 +201,10 @@ public class UserProfileService(UserProfileRepository userProfileRepository, Rol
         catch (Exception ex)
         {
             Debug.WriteLine("ERROR :: " + ex.Message);
-            // Logga eller hantera felet på lämpligt sätt
-            throw; // Rethrow exception för att låta det någon högre upp hantera
+            
+            throw; 
         }
+
     }
 
 
@@ -222,118 +212,5 @@ public class UserProfileService(UserProfileRepository userProfileRepository, Rol
 
 
 }
-//public bool CreateUser(User user)
-//{
-//    try
-//    {
-//        if (!_userRepository.Exists(x => x.Email == user.Email))
-//        {
 
-//            var userProfile = _userRepository.GetOne(x => x.Email == user.Email);
-//            if(userProfile == null)
-//            {
-//                var userEntity = new UserEntity
-//                {
-
-//                    Email = user.Email,
-//                    Password = user.Password,
-
-//                };
-//                var result = _userRepository.Create(userEntity);
-//                if (result != null)
-//                {
-//                    return true;
-//                }
-
-//            }
-
-//            return false;
-
-
-//        }
-
-//    }
-//    catch (Exception ex)
-//    {
-//        Debug.WriteLine("ERROR :: " + ex.Message);
-//    }
-//    return false;
-//}
-//public bool CreateRoleProfile(RoleDto role)
-//{
-//    try
-//    {
-//        if (!_roleRepository.Exists(x => x.Admin == role.Admin))
-//        {
-
-//            var roleEntity = _roleRepository.GetOne(x => x.Admin == role.Admin);
-//            if (roleEntity == null)
-//            {
-//                var roleProfile = new RoleEntity
-//                {
-
-//                    Admin = role.Admin,
-//                    Private = role.Private,
-
-//                };
-//                var result = _roleRepository.Create(roleProfile);
-//                if (result != null)
-//                {
-//                    return true;
-//                }
-
-//            }
-
-//            return false;
-
-
-//        }
-
-//    }
-//    catch (Exception ex)
-//    {
-//        Debug.WriteLine("ERROR :: " + ex.Message);
-//    }
-//    return false;
-//}
-//public bool CreateAdress(AdressDto adress)
-//{
-//    try
-//    {
-//        if (!_userRepository.Exists(x => x.Id == adress.Id))
-//        {
-
-//            var adressProfile = _userRepository.GetOne(x => x.Id == adress.Id);
-//            if (adressProfile == null)
-//            {
-//                var adressEntity = new AdressEntity
-//                {
-//                    City = adress.City,
-//                    StreetName = adress.StreetName,
-//                    PostalCode = adress.PostalCode,
-//                    Country = adress.Country,
-//                    PhoneNumber = adress.PhoneNumber
-
-
-//                };
-//                var result = _adressRepository.Create(adressEntity);
-//                if (result != null)
-//                {
-//                    return true;
-//                }
-
-//            }
-
-//            return false;
-
-
-//        }
-
-//    }
-//    catch (Exception ex)
-//    {
-//        Debug.WriteLine("ERROR :: " + ex.Message);
-//    }
-//    return false;
-//}
 
